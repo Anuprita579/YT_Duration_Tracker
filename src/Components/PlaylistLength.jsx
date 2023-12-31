817106d093c0ab414801a7c08b9bad5dc04c58d6
Anuprita <anupritamhapankar22@gmail.com>
2024-01-01 00:09:35 +0530

    updated commit

--- src/Components/PlaylistLength.jsx
+++ src/Components/PlaylistLength.jsx
@@ -3,12 +3,51 @@ import axios from 'axios';
 
 function PlaylistLength({playlistId}) {
   const [playlistItem, setPlaylistItem] = useState({items: []});
+  const [duration, setDuration] = useState(0);
   
   useEffect(()=>{
-    axios
-      .get(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=contentDetails&maxResults=5000&playlistId=${playlistId}&key=${import.meta.env.VITE_API_KEY}`)
-      .then((res)=> setPlaylistItem(res.data));
+    const fetchPlaylistData = async() =>{
+      try{
+        const playlistResponse = await axios.get(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=contentDetails&maxResults=5000&playlistId=${playlistId}&key=${import.meta.env.VITE_API_KEY}`);
+        const playlistData = playlistResponse.data;
+        setPlaylistItem(playlistData);
+
+        const videoIds = playlistData.items.map(item => item.contentDetails.videoId);
+        const videoDetails = await Promise.all(
+          videoIds.map(async videoId => {
+            const videoResponse = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails&id=${videoId}&key=${import.meta.env.VITE_API_KEY}`);
+            const videoData = videoResponse.data;
+            return videoData?.items[0]?.contentDetails?.duration;
+          })
+        );
+
+        const totalDurationInSeconds = videoDetails.reduce((total, duration)=>{
+          return total + convertDurationToSeconds(duration);
+        }, 0);
+        setDuration(totalDurationInSeconds);
+      }
+      catch{
+        console.log("Error ")
+      }  
+    }
+
+  fetchPlaylistData();
   },[playlistId]);
+
+  const convertDurationToSeconds = (duration) => {
+    const match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+    const hours = parseInt(match[1]) || 0;
+    const minutes = parseInt(match[2]) || 0;
+    const seconds = parseInt(match[3]) || 0;
+    return hours*3600 + minutes *60 + seconds;
+  }
+
+  const formatDuration = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds/3600);
+    const minutes = Math.floor((totalSeconds%3600) / 60);
+    const seconds = totalSeconds%60;
+    return `${hours}h ${minutes}m ${seconds}`;
+  }
 
   const post = playlistItem.items[0];
   
@@ -16,7 +55,7 @@ function PlaylistLength({playlistId}) {
     <>
       <div className='playlist-length' key={post?.id}>
         <h3>Number of Videos : <span> {playlistItem?.pageInfo?.totalResults}</span> </h3>
-        <h3>Total Duration : <span> 1hr 45 min</span> </h3>
+        <h3>Total Duration : <span> {formatDuration(duration)}</span> </h3>
         <h3>Youtuber : <span> {post?.snippet?.channelTitle}</span> </h3>
       </div>    
     </>
